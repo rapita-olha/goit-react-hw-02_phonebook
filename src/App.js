06c3cbeb6f97aa681b00c7b2da092d0c47f1c621
Olha <gorokhovianka@gmail.com>
2022-09-29 20:56:07 +0300

    hw_2

---
+++ src/App.js
@@ -0,0 +1,77 @@
+import React, { Component } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+
+import Title from './components/Title';
+import ContactForm from './components/ContactForm';
+import Filter from './components/Filter';
+import ContactList from './components/ContactList';
+
+import './App.scss';
+
+class App extends Component {
+  // стейт нужен каждый раз, поэтому храним в родителе
+  state = {
+    contacts: [],
+    filter: '',
+  };
+
+  addContact = ({ name, number }) => {
+    const { contacts } = this.state;
+    const contact = {
+      id: uuidv4(),
+      name,
+      number,
+    };
+
+    contacts.some(contact => contact.name.toLowerCase() === name.toLowerCase())
+      ? alert(`${name} is already in contacts.`)
+      : //   alert(`${contact.name} is already in contacts.`);
+        this.setState(prevState => ({
+          contacts: [contact, ...prevState.contacts],
+        }));
+  };
+
+  deleteContact = id => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(contact => contact.id !== id),
+    }));
+  };
+
+  changeFilter = e => {
+    this.setState({ filter: e.currentTarget.value });
+  };
+
+  visibleContacts = () => {
+    const { contacts, filter } = this.state;
+
+    return contacts.filter(
+      contact =>
+        contact.name.toLowerCase().includes(filter.toLowerCase()) ||
+        contact.number.includes(filter),
+    );
+  };
+
+  render() {
+    const { filter } = this.state;
+    const { addContact, changeFilter, deleteContact, visibleContacts } = this;
+  
+    console.log(this.state.contacts);
+    return (
+      <div className="container">
+        <h1 className="visually_hidden">Phonebook</h1>
+
+        <Title title="Phonebook" />
+        <ContactForm onSubmit={addContact} />
+
+        <Title title="Contacts" />
+        <Filter value={filter} onChange={changeFilter} />
+        <ContactList
+          visibleContacts={visibleContacts}
+          onDeleteContact={deleteContact}
+        />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Filter/Filter.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import s from './Filter.module.scss';
+
+const Filter = ({ value, onChange }) => (
+  <label className={s.label}>
+    Find contacts by name
+    <input
+      type="text"
+      value={value}
+      onChange={onChange}
+      className={s.input}
+    ></input>
+  </label>
+);
+
+Filter.propTypes = {
+  value: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+};
+
+export default Filter;
